41f290e4d9aea8af9254a24f02967457bf06734d
Pratik Dahal <notpratikdahal@gmail.com>
2025-10-16 09:47:30 +0545

    new commit

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class employee(models.Model):
+    eno=models.IntegerField()
+    ename=models.CharField(max_length=255)
+    esal=models.IntegerField()
+    eaddr=models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.ename
